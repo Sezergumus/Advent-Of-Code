3bcd70b4bb1cf6948b1f316dcaef10f9c6a182bf
Sezer Gümüş <sezer2004@gmail.com>
2024-12-12 12:50:58 +0300

    graph solution

--- day-05/index.js
+++ day-05/index.js
@@ -19,8 +19,6 @@ rules.forEach(([from, to]) => {
   graph[from].push(to);
 });
 
-console.log(graph);
-
 // variable for part two
 const invalidUpdates = [];
 
@@ -50,4 +48,62 @@ updates.forEach((update) => {
     const middlePage = update[Math.floor(update.length / 2)];
     sumOfMiddles += middlePage;
   }
+});
+
+let correctedSumOfMiddles = 0;
+
+const topologicalSort = (update, graph) => {
+  const inDegree = {};
+  const result = [];
+  const queue = [];
+
+  update.forEach((node) => {
+    inDegree[node] = 0;
+  });
+
+  update.forEach((node) => {
+    if (graph[node]) {
+      graph[node].forEach((neighbor) => {
+        if (inDegree[neighbor] !== undefined) {
+          inDegree[neighbor] += 1;
+        }
+      });
+    }
+  });
+
+  update.forEach((node) => {
+    if (inDegree[node] === 0) {
+      queue.push(node);
+    }
+  });
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    result.push(current);
+
+    if (graph[current]) {
+      graph[current].forEach((neighbor) => {
+        if (inDegree[neighbor] !== undefined) {
+          inDegree[neighbor] -= 1;
+
+          if (inDegree[neighbor] === 0) {
+            queue.push(neighbor);
+          }
+        }
+      });
+    }
+  }
+
+  if (result.length === update.length) {
+    return result;
+  } else {
+    throw new Error("Topological sort failed. Graph contains a cycle.");
+  }
+};
+
+invalidUpdates.forEach((update) => {
+  const sortedUpdate = topologicalSort(update, graph);
+
+  const middlePage = sortedUpdate[Math.floor(sortedUpdate.length / 2)];
+  correctedSumOfMiddles += middlePage;
 });
